6feacb36ee0c78d284b5171834c2e31c2127ff3d
WaynePL <wayneplivingston@gmail.com>
2018-10-28 19:32:56 -0400

    carousel added to projects

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,22 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
 
+next.addEventListener('click', () => {
+  plusSlides(1)
+})
+
+prev.addEventListener('click', () => {
+  plusSlides(-1)
+})
+
+const dots = document.querySelectorAll('.dot');
+dots.forEach(element => {
+  element.addEventListener('click', element => {
+    currentSlide(element.target.getAttribute('data-num'));
+  }) 
+})
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
---
+++ static/test/dom.test.js
@@ -0,0 +1,4 @@
+
+describe('image carousel', () => {
+	test
+}
