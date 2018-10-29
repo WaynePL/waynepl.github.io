e13f1f7cb9053132a73b285e8bb6874a3b57b053
WaynePL <wayneplivingston@gmail.com>
2018-10-29 01:12:26 -0400

    added navigation menu popout and about me modal

--- static/js/dom.js
+++ static/js/dom.js
@@ -1,1 +1,17 @@
-dom.js
+
+
+function setToBlock(element) {
+	element.style.display = 'block';
+}
+
+function setToFlex(element) {
+	element.style.display = 'flex';
+}
+
+function setToNone(element) {
+	element.style.display = 'none';
+}
+module.exports = {
+	setToFlex,
+	setToNone
+}
--- static/test/dom.test.js
+++ static/test/dom.test.js
@@ -1,4 +1,18 @@
+const { setToFlex, setToNone } = require('../js/dom.js')
+describe('navigation popout', () => {
+	test('turn on nav', () => {
+		const mainNav = document.createElement('nav');
+		mainNav.classList.add('main__nav'); 
+		setToFlex(mainNav);
 
-describe('image carousel', () => {
-	test
-}
+		expect(mainNav.style.display).toEqual('flex');
+	})
+
+	test('turns off nav', () => {
+		const mainNav = document.createElement('nav');
+		mainNav.classList.add('main__nav'); 	
+		setToNone(mainNav);
+
+		expect(mainNav.style.display).toEqual('none');
+	})
+})
